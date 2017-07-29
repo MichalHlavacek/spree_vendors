9795238227d1946382fd86873d3a596112c965e3
Michal Hlavacek <hello@michalhlavacek.com>
2017-07-29 16:52:50 +0200

    New Product Admin template fix

--- app/overrides/add_new_product_user_id.rb
+++ app/overrides/add_new_product_user_id.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(virtual_path: 'spree/admin/products/new',
   name: 'add_new_product_user_id',
   insert_top: "[data-hook='new_product']",
-  text: '<% if spree_current_user.has_spree_role?("manager") %><%= f.hidden_field :spree_vendor_id %><% else %><%= f.label :spree_vendor_id, "Vendor ID" %><%= f.text_field :spree_vendor_id, :class => "form-control" %><% end %>')
+  text: '<% if spree_current_user.has_spree_role?("manager") %><%= f.hidden_field :vendor_id %><% else %><%= f.label :vendor_id, "Vendor ID" %><%= f.text_field :vendor_id, :class => "form-control" %><% end %>')
